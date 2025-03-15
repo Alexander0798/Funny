445186db649ea3be1be55b239893432d7da64a2e
Alexander0798 <rav89515139989@yandex.ru>
2025-03-15 15:33:51 +0300

    add gallery and feedback slider

--- index.js
+++ index.js
@@ -1,16 +1,30 @@
 import Swiper from 'https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.mjs'
 
-const swiper = new Swiper('.swiper',
+const gallerySwiper = new Swiper('.gallery__swiper',
+  {
+    slidesPerView: 3.2,
+    spaceBetween: 20,
+    freeMode: true,
+    pagination: {
+      el: ".gallery__swiper-pagination",
+      clickable: true,
+    },
+    navigation: {
+      nextEl: '.gallery__swiper-button-next',
+      prevEl: '.gallery__swiper-button-prev',
+    },
+  });
+const fedbackSwiper = new Swiper('.feedback__swiper',
   {
     slidesPerView: 1.4,
     spaceBetween: 80,
     freeMode: true,
     pagination: {
-      el: ".swiper-pagination",
+      el: ".feedback__swiper-pagination",
       clickable: true,
     },
     navigation: {
-      nextEl: '.swiper-button-next',
-      prevEl: '.swiper-button-prev',
+      nextEl: '.feedback__swiper-button-next',
+      prevEl: '.feedback__swiper-button-prev',
     },
   });
